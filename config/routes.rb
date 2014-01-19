646a66a64296e6d24133f2a88d1ab10c0304aba3
chris <xanatosg0180@gmail.com>
2014-01-19 15:36:07 -0500

    created test page and added buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" #creates about path
+  get "test" => "pages#test"
   # get "home" => "pages#home" #
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
